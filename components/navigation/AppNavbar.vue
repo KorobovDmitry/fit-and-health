144b990bbebcd328917371edccfdf7af54938f07
dm.dev@icmy.ru <yug0cod@furd0GHIY>
2020-12-11 12:06:39 +0400

    update-from-11-12-2020

--- components/navigation/AppNavbar.vue
+++ components/navigation/AppNavbar.vue
@@ -4,64 +4,27 @@
       <nuxt-link to="/" exact no-prefetch>FH</nuxt-link>
     </div>
 
-    <nuxt-link
-      to="/profile"
-      exact
-      no-prefetch
-      class="ti-home navbar__page-link"
-      active-class="navbar__page-link--active"
-    ></nuxt-link>
-    <nuxt-link
-      to="/messages"
-      exact
-      no-prefetch
-      class="ti-comments navbar__page-link"
-      active-class="navbar__page-link--active"
-    ></nuxt-link>
-    <nuxt-link
-      to="/training-diary"
-      no-prefetch
-      class="ti-timer navbar__page-link"
-      active-class="navbar__page-link--active"
-    ></nuxt-link>
-    <nuxt-link
-      to="/meal-planer"
-      no-prefetch
-      class="ti-agenda navbar__page-link"
-      active-class="navbar__page-link--active"
-    ></nuxt-link>
-    <nuxt-link
-      to="/challenge"
-      no-prefetch
-      class="ti-cup navbar__page-link"
-      active-class="navbar__page-link--active"
-    ></nuxt-link>
-    <nuxt-link
-      to="/measurements"
-      exact
-      no-prefetch
-      class="ti-bar-chart navbar__page-link"
-      active-class="navbar__page-link--active"
-    ></nuxt-link>
-
-    <nuxt-link
-      to="/handbook"
-      exact
-      no-prefetch
-      class="ti-bookmark-alt navbar__page-link"
-      active-class="navbar__page-link--active"
-    ></nuxt-link>
-
-    <nuxt-link
-      to="/settings"
-      exact
-      no-prefetch
-      class="ti-settings navbar__page-link"
-      active-class="navbar__page-link--active"
-    ></nuxt-link>
+    <div
+      v-for="(linkGroup, index) in mainNav"
+      :key="index"
+      class="navbar__page-links-group"
+      :class="[{'navbar__page-links-group--active': linkGroup.active}]"
+      @click="updateGroupHeight($event)"
+    >
+      <nuxt-link
+        v-for="(item, index) in linkGroup.links"
+        :key="index"
+        :to="item.link"
+        exact
+        no-prefetch
+        class="navbar__page-link"
+        :class="item.icon"
+        active-class="navbar__page-link--active"
+      ></nuxt-link>
+    </div>
 
     <i
-      class="ti-export navbar__page-link logout"
+      class="ti-export logout"
       @click="logout()"
     ></i>
   </nav>
@@ -70,13 +33,150 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      mainNav: [
+        {
+          active: false,
+          group: 'profile',
+          links: [
+            {
+              link: '/profile',
+              icon: 'ti-home'
+            },
+            {
+              link: '/profile/messages',
+              icon: 'ti-comments'
+            }
+          ]
+        },
+        {
+          active: false,
+          group: 'training-diary',
+          links: [
+            {
+              link: '/training-diary',
+              icon: 'ti-timer'
+            },
+            {
+              link: '/training-diary/training-programs',
+              icon: 'ti-clipboard'
+            },
+            {
+              link: '/training-diary/exercise-guide',
+              icon: 'ti-book'
+            }
+          ]
+        },
+        {
+          active: false,
+          group: 'meal-planer',
+          links: [
+            {
+              link: '/meal-planer',
+              icon: 'ti-agenda'
+            },
+            {
+              link: '/meal-planer/meal-plans',
+              icon: 'ti-clipboard'
+            },
+            {
+              link: '/meal-planer/recipes-book',
+              icon: 'ti-book'
+            },
+            {
+              link: '/meal-planer/food-calorie-table',
+              icon: 'ti-view-list-alt'
+            }
+          ]
+        },
+        {
+          active: false,
+          group: 'challenges',
+          links: [
+            {
+              link: '/challenges',
+              icon: 'ti-cup'
+            }
+          ]
+        },
+        {
+          active: false,
+          group: 'measurements',
+          links: [
+            {
+              link: '/measurements',
+              icon: 'ti-bar-chart'
+            }
+          ]
+        },
+        {
+          active: false,
+          group: 'handbook',
+          links: [
+            {
+              link: '/handbook',
+              icon: 'ti-bookmark-alt'
+            }
+          ]
+        },
+        {
+          active: false,
+          group: 'settings',
+          links: [
+            {
+              link: '/settings',
+              icon: 'ti-settings'
+            }
+          ]
+        },
+      ]
+    }
+  },
+  watch: {
+    $route(to, from) {
+      const path = to.path
+      const PathChunks = path.split('/').filter(item => item !== "")
+
+      this.mainNav.forEach(element => {
+        element.group === PathChunks[0] ? element.active = true : element.active = false
+      })
+    }
   },
   methods: {
+    updateGroupHeight ($event) {
+      const LinksGroups = document.querySelectorAll('.navbar__page-links-group')
+      const LinkHeight = document.querySelector('.navbar__page-link').getBoundingClientRect().height
+      for (let i = 0; i < LinksGroups.length; i++) {
+        LinksGroups[i].style.height = LinkHeight + 4 + 'px'
+      }
+
+      const ActiveLinksGroupLinks = $event.target.parentElement.querySelectorAll('.navbar__page-link')
+      $event.target.parentElement.style.height = LinkHeight * ActiveLinksGroupLinks.length + 4 + 'px'
+    },
     logout () {
       this.$store.dispatch('auth/logout')
       this.$router.push('/auth')
-    }
+    },
+  },
+  mounted () {
+    const Path = this.$route.path
+    const PathChunks = Path.split('/').filter(item => item !== "")
+
+    this.mainNav.forEach(element => {
+      element.group === PathChunks[0] ? element.active = true : element.active = false
+    })
+
+    this.$nextTick(function () {
+      const LinksGroups = document.querySelectorAll('.navbar__page-links-group')
+      const LinkHeight = document.querySelector('.navbar__page-link').getBoundingClientRect().height
+      for (let i = 0; i < LinksGroups.length; i++) {
+        LinksGroups[i].style.height = LinkHeight + 4 + 'px'
+      }
+
+      const ActiveLinksGroup = document.querySelector('.navbar__page-links-group--active')
+      const ActiveLinksGroupLinks = ActiveLinksGroup.querySelectorAll('.navbar__page-link')
+      ActiveLinksGroup.style.height = LinkHeight * ActiveLinksGroupLinks.length + 4 + 'px'
+    })
   }
 }
 </script>
@@ -102,6 +202,7 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
+    margin-bottom: auto;
     width: 100%;
     height: 60px;
     background: $green;
@@ -111,34 +212,51 @@ export default {
       font-weight: 600;
     }
   }
-  .navbar__page-link {
-    // border: 1px solid red;
-    position: relative;
-    margin: 5px 0;
+  .navbar__page-links-group {
+    display: flex;
+    flex-direction: column;
+    border: 2px solid transparent;
+    border-radius: 8px;
+    transition: $tr-02;
+    height: 0;
+    overflow: hidden;
+    .navbar__page-link {
+      // border: 1px solid red;
+      position: relative;
+      padding: 10px;
+      text-align: center;
+      text-decoration: none;
+      color: $black;
+      font-size: 28px;
+      border-left: 2px solid transparent;
+      border-right: 2px solid transparent;
+      transition: $tr-02;
+    }
+    .navbar__page-link:last-child {
+      margin-bottom: 0;
+    }
+    .navbar__page-link:hover {
+      color: $green;
+    }
+    .navbar__page-link--active {
+      color: $white;
+      background: $green;
+    }
+    .navbar__page-link--active:hover {
+      color: $white;
+    }
+  }
+
+  .navbar__page-links-group--active {
+    border: 2px solid $green;
+  }
+  
+  .logout {
+    margin: auto 0 10px 0;
     padding: 10px;
-    text-align: center;
-    text-decoration: none;
+    transform: rotate(90deg);
     color: $black;
     font-size: 28px;
-    border-left: 2px solid transparent;
-    border-right: 2px solid transparent;
-    cursor: pointer;
-    transition: $tr-02;
-  }
-  .navbar__page-link:nth-child(2) {
-    margin-top: auto;
-  }
-  .navbar__page-link:last-child {
-    margin-top: auto;
-    margin-bottom: 20px;
-  }
-  .navbar__page-link--active {
-    color: $white;
-    background: $green;
-    border-radius: 6px;
-  }
-  .logout {
-    transform: rotate(90deg);
   }
 }
 
